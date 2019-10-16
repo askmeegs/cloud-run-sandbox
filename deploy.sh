8cada5f66ffc2141a9b9b15d210d1e4758e9eada
askmeegs <megan037@gmail.com>
2019-10-16 18:06:09 -0400

    cluster and region

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 for i in manifests/*; do
   service_name=`basename $i .yaml`
   echo $service_name
-  gcloud alpha run services replace $i  --platform gke
+  gcloud alpha run services replace $i  --platform gke --cluster gcp --cluster-location us-central1-b
 done
