13eddc9b541179ab6fbe15dd999f0a205579cede
askmeegs <megan037@gmail.com>
2019-10-16 18:07:55 -0400

    Remove redis from cloud run, deploy in GKE

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-for i in manifests/*; do
+
+kubectl apply -f redis.yaml
+sleep 10
+
+for i in services/*; do
   service_name=`basename $i .yaml`
   echo $service_name
   gcloud alpha run services replace $i  --platform gke --cluster gcp --cluster-location us-central1-b
